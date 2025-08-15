786f3e3279467da91f163fd1ea4a734bc62cf867
Ramen361 <ch3353c4k3ss@gmail.com>
2025-08-15 18:01:36 -0400

    change JSJSJSJ

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ countButton.addEventListener('click', function() {
     let current = parseInt(countButton.textContent, 10);
     countButton.textContent = current + 1;
 });
+let clicks = parseInt(localStorage.getItem('clicks')) || 0;
+
+
+function updateClickDisplay() {
+    document.getElementById('click-count').textContent = clicks;
+}
+
+
+document.getElementById('click-btn').addEventListener('click', function() {
+    clicks++;
+    localStorage.setItem('clicks', clicks); 
+    updateClickDisplay();
+});
+
+
+updateClickDisplay();
