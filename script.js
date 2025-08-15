3e05dc2d2308d816c8ef7a9b8538de5a564c4d4e
Ramen361 <ch3353c4k3ss@gmail.com>
2025-08-15 17:39:00 -0400

    change js and html

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+const countButton = document.getElementById('count');
+
+countButton.addEventListener('click', function() {
+    let current = parseInt(countButton.textContent, 10);
+    countButton.textContent = current + 1;
+});
