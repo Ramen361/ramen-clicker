888dbd8249d68624f2b77ed5997061d54fce9a6c
Ramen361 <ch3353c4k3ss@gmail.com>
2025-08-15 20:37:49 -0400

    upgrade button

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ if (isNaN(clicks)) clicks = 0;
 let upgradeCost = parseInt(localStorage.getItem('upgradeCost'), 10);
 if (isNaN(upgradeCost)) upgradeCost = 100;
 
-// New: load clickValue (how many clicks per click)
+
 let clickValue = parseFloat(localStorage.getItem('clickValue'));
 if (isNaN(clickValue)) clickValue = 1;
 
@@ -31,7 +31,7 @@ upgradeButton.addEventListener('click', function() {
     if (clicks >= upgradeCost) {
         clicks -= upgradeCost;
         upgradeCost = Math.ceil(upgradeCost * 1.15);
-        clickValue = parseFloat((clickValue * 1.10).toFixed(2)); // Increase by 10%
+        clickValue = clickValue * 2; // Double the click value
         localStorage.setItem('clicks', clicks);
         localStorage.setItem('upgradeCost', upgradeCost);
         localStorage.setItem('clickValue', clickValue);
@@ -42,17 +42,3 @@ upgradeButton.addEventListener('click', function() {
 
 updateClickDisplay();
 updateUpgradeCostDisplay();
-
-
-upgradeButton.addEventListener('click', function() {
-    if (clicks >= upgradeCost) {
-        clicks -= upgradeCost;
-        upgradeCost = Math.ceil(upgradeCost * 1.15);
-        clickValue = clickValue * 2; // Double the click value
-        localStorage.setItem('clicks', clicks);
-        localStorage.setItem('upgradeCost', upgradeCost);
-        localStorage.setItem('clickValue', clickValue);
-        updateClickDisplay();
-        updateUpgradeCostDisplay();
-    }
-});
